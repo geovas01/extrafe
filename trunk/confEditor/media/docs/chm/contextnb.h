dcb839f48a763edc0c11b7fffcc2f4f39ad3a560
spoooky11@yahoo.com <spoooky11@yahoo.com@0e7a996f-2efb-5a92-0084-676697e29713>
2012-03-20 12:46:20 +0000

    Delete to reenter as on svn file

--- trunk/confEditor/media/docs/chm/contextnb.h
+++
@@ -1,9 +0,0 @@
-#define Introduction 0
-#define Welcome 1
-#define Whatsnew 2
-#define GettingStarted 3
-#define Systemrequirements 4
-#define Gettinghelp 5
-#define History 6
-#define ExtraFe020Meleti 7
-#define ExtraFe010Aiodi 8
